1463729e21533baa83b119d29bf6a272bc044fdd
fritih-maya <163916246+fritih-maya@users.noreply.github.com>
2025-02-05 14:13:35 +0100

    hey

---
+++ safex/auth_app/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm 
+from .form import CustomUserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def inscription(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Enregistrement du nouvel utilisateur
+            login(request, user)  # Connexion automatique de l'utilisateur après l'inscription
+            return redirect('acceuil')  # Redirection vers la page d'accueil
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'inscription.html', {'form': form})
+
+
+def connexion(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('acceuil')
+        else:
+            messages.error(request, 'Nom d\'utilisateur ou mot de passe incorrect.')
+    return render(request, 'connexion.html')
+
+@login_required
+def acceuil(request):
+    return render(request, 'acceuil.html')
+
+def deconnexion(request):
+    logout(request)
+    return redirect('connexion')
