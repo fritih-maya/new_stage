fe3e7fb3834c529e9001aeef0aaece8ea6c67c2e
fritih-maya <163916246+fritih-maya@users.noreply.github.com>
2025-02-07 21:05:28 +0100

    page_admin+page_connexion

--- safex/auth_app/views.py
+++ safex/auth_app/views.py
@@ -1,29 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm 
-from .form import CustomUserCreationForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 
-
-# Create your views here.
-def inscription(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()  # Enregistrement du nouvel utilisateur
-            login(request, user)  # Connexion automatique de l'utilisateur après l'inscription
-            return redirect('acceuil')  # Redirection vers la page d'accueil
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'inscription.html', {'form': form})
-
 User = get_user_model()
 
 def connexion(request):
     if request.method == 'POST':
-        email = request.POST['email'] 
+        email = request.POST['email']
         password = request.POST['password']
 
         # Vérifier si un utilisateur existe avec cet email
@@ -33,15 +18,16 @@ def connexion(request):
             messages.error(request, "Utilisateur non trouvé.")
             return render(request, 'connexion.html')
 
-        # Authentifier l'utilisateur
-        user = authenticate(request, username=user.name_user, password=password) 
+        # Authentifier l'utilisateur avec l'email en tant qu'identifiant
+        user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
             return redirect('acceuil')  # Redirige vers la page d'accueil
         else:
             messages.error(request, "Mot de passe incorrect.")
-    
+
     return render(request, 'connexion.html')
+
 
 @login_required
 def acceuil(request):
--- safex/safex/urls.py
+++ safex/safex/urls.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, re_path
+from django.shortcuts import redirect
 from auth_app import views
 from auth_app.admin import safex
 
 urlpatterns = [
+    # Page d'administration personnalisée
     path('admin/', safex.urls),
-    path('', views.inscription, name='inscription'),
+
+    # Routes utilisateur
     path('connexion/', views.connexion, name='connexion'),
     path('acceuil/', views.acceuil, name='acceuil'),
-    path('deconnexion/',views.deconnexion, name='deconnexion'),
+    path('deconnexion/', views.deconnexion, name='deconnexion'),
 
+    # Redirection de l'URL racine vers la page de connexion
+    path('', lambda request: redirect('connexion')),
 ]
-
